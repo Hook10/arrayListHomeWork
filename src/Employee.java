719a79c2b53619ccd7a8d996d9d10608bd639c13
Hook10 <sapasask@gmail.com>
2020-02-26 19:07:10 +0600

    firstCommit

---
+++ src/Employee.java
@@ -0,0 +1,126 @@
+import java.util.Comparator;
+
+public class Employee implements Comparable<Employee> {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private String position;
+
+    public Employee(int id, String firstName, String lastName, int salary, String position) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(!(obj instanceof Employee)) return false;
+        Employee employee = (Employee)obj;
+        if(!this.firstName.equals(firstName)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", position='" + position + '\'' +
+                '}';
+    }
+
+    public static Comparator<Employee> getCompByName(){
+        return new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return (o1.firstName.compareTo(o2.firstName));
+
+            }
+        };
+    }
+    public static Comparator<Employee> getCompByPosition(){
+        return new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return (o1.position.compareTo(o2.position));
+
+            }
+        };
+    }
+
+    public static Comparator<Employee> getCompById() {
+        return new Comparator<Employee>() {
+
+            public int compare (Employee employee1, Employee employee2){
+                Integer id1 = employee1.getId();
+                Integer id2 = employee2.getId();
+                return id1.compareTo(id2);
+            }
+
+        };
+
+    }
+    public static Comparator<Employee> getCompBySalary() {
+        return new Comparator<Employee>() {
+
+            public int compare (Employee employee1, Employee employee2){
+                Integer id1 = employee1.getId();
+                Integer id2 = employee2.getId();
+                return id2.compareTo(id1);
+            }
+
+        };
+
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return this.id - id;
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,66 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Runner {
+    public static void main(String[] args){
+        ArrayList<Employee> topManagement = new ArrayList<>();
+        Employee director = new Employee(1001, "Konstantin", "Ernst", 100000, "director" );
+        topManagement.add(director);
+
+        ArrayList<Employee> midManagement = new ArrayList<>();
+        Employee managerFirst = new Employee(1002, "Mikhail", "Shifutinsky", 80000, "department Manager" );
+        Employee managerSecond = new Employee(1003, "Ivan", "Urgant", 85000, "department Manager" );
+        midManagement.add(managerFirst);
+        midManagement.add(managerSecond);
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        Employee employee1 = new Employee(1004, "Maria", "Sharapova", 50000, "engineer" );
+        Employee employee2 = new Employee(1005, "Philip", "Kirkorov", 50000, "engineer" );
+        Employee employee3 = new Employee(1006, "Gregor", "Leps", 50000, "engineer" );
+        Employee employee4 = new Employee(1007, "Dima", "Belan", 50000, "engineer" );
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+        employees.add(employee4);
+
+        midManagement.addAll(employees);
+        topManagement.addAll(midManagement);
+
+        Collections.sort(topManagement, Employee.getCompByName());
+        for(Employee employee : topManagement){
+            System.out.println(employee.getFirstName());
+        }
+        System.out.println("--------" + "\n");
+
+        Collections.sort(topManagement, Employee.getCompByName());
+        for(Employee employee : topManagement){
+            System.out.println(employee.getLastName());
+        }
+        System.out.println("--------" + "\n");
+
+        Collections.sort(topManagement, Employee.getCompByPosition());
+        for(Employee employee : topManagement){
+            System.out.println(employee.getPosition());
+        }
+        System.out.println("--------" + "\n");
+
+        Collections.sort(topManagement, Employee.getCompById());
+        for(Employee employee : topManagement){
+            System.out.println(employee.getId());
+        }
+        System.out.println("--------" + "\n");
+
+        Collections.sort(topManagement, Employee.getCompBySalary());
+        for(Employee employee : topManagement){
+            System.out.println(employee.getSalary());
+        }
+
+
+
+
+
+
+    }
+}
